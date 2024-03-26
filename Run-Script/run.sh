330858d86ce53f3c558c97183b78b2646901ff60
hitfyd <hitfyd@foxmail.com>
2024-03-26 21:59:15 +0800

    暂时不对比IMD，评价指标计算存在错误

--- Run-Script/run.sh
+++ Run-Script/run.sh
@@ -1,15 +1,15 @@
 # CamCAN
-python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_hgrn.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/KD_varcnn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_varcnn.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_hgrn.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/KD_varcnn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_varcnn.yaml EXPLAINER.TYPE SeparateSurrogate
 
-python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_hgrn.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_sdt.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/KD_varcnn_sdt.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_sdt.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_varcnn.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_hgrn.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_sdt.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/KD_varcnn_sdt.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_sdt.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_varcnn.yaml EXPLAINER.TYPE IMDExplainer
 
 python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_hgrn.yaml EXPLAINER.TYPE DeltaExplainer
 python difference_analysis.py --cfg ../configs/CamCAN/Before_Distill/varcnn_sdt.yaml EXPLAINER.TYPE DeltaExplainer
@@ -25,17 +25,17 @@ python difference_analysis.py --cfg ../configs/CamCAN/After_Distill/FAKD_varcnn_
 
 
 # DecMeg2014
-python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/lfcnn_varcnn.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/hgrn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/KD_hgrn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
-python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_hgrn.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/lfcnn_varcnn.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/hgrn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/KD_hgrn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_sdt.yaml EXPLAINER.TYPE SeparateSurrogate
+#python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_hgrn.yaml EXPLAINER.TYPE SeparateSurrogate
 
-python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/lfcnn_varcnn.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/hgrn_sdt.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/KD_hgrn_sdt.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_sdt.yaml EXPLAINER.TYPE IMDExplainer
-python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_hgrn.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/lfcnn_varcnn.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/hgrn_sdt.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/KD_hgrn_sdt.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_sdt.yaml EXPLAINER.TYPE IMDExplainer
+#python difference_analysis.py --cfg ../configs/DecMeg2014/After_Distill/FAKD_hgrn_hgrn.yaml EXPLAINER.TYPE IMDExplainer
 
 python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/lfcnn_varcnn.yaml EXPLAINER.TYPE DeltaExplainer
 python difference_analysis.py --cfg ../configs/DecMeg2014/Before_Distill/hgrn_sdt.yaml EXPLAINER.TYPE DeltaExplainer
