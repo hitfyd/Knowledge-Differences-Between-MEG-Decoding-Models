d467c988b3249ff4d8f06c8ee4229315b1a1a970
hitfyd <hitfyd@foxmail.com>
2024-04-03 11:21:33 +0800

    增加Boruta特征选择器

---
+++ differlib/feature_selection/borutaFS.py
@@ -0,0 +1,24 @@
+import numpy as np
+from boruta import BorutaPy
+from sklearn.ensemble import RandomForestClassifier
+
+from .fsm import FSMethod
+
+
+class BorutaFS(FSMethod):
+    def __init__(self):
+        super(BorutaFS, self).__init__()
+        perc = 80
+        rf = RandomForestClassifier(n_jobs=-1, class_weight='balanced', max_depth=5)
+        self.method = BorutaPy(rf, n_estimators='auto', perc=perc, alpha=0.05, two_step=True, max_iter=100, verbose=2)
+        self.contributions = None
+
+    def fit(self, x: np.ndarray, pred_target_A, pred_target_B, **kwargs):
+        delta_target = pred_target_A ^ pred_target_B
+        self.method.fit(x, delta_target)
+
+    def computing_contribution(self, *argv, **kwargs):
+        pass
+
+    def transform(self, x: np.ndarray, **kwargs):
+        return self.method.transform(x)
