e0f0d6ef7cd1e1edb18c734507b88144a0493576
hitfyd <hitfyd@foxmail.com>
2024-05-07 10:26:55 +0800

    加权平均有效；更小的window_length和更大的M有较小提升；多线程实现并未实现加速，尝试在n_samples上进行平行计算，涉及到GPU分配

--- differlib/feature_selection/DiffShapleyFS.py
+++ differlib/feature_selection/DiffShapleyFS.py
@@ -9,7 +9,7 @@ from ..engine.utils import predict
 
 
 class DiffShapleyFS(FSMethod):
-    def __init__(self, parallel=True, n_jobs=64):
+    def __init__(self, parallel=True, n_jobs=8):
         super(DiffShapleyFS, self).__init__()
         self.parallel = parallel
         self.n_jobs = n_jobs
@@ -60,48 +60,51 @@ class DiffShapleyFS(FSMethod):
         return x[:, ind]
 
 
-def feature_segment(channels, points, window_length):
-    channel_windows_num = int(points / window_length)  # 需要计算的通道特征数和时间特征数，总特征数为c_features x p_features
-    features_num = channels * channel_windows_num
-    channel_list, point_start_list = [], []
-    for feature_id in range(features_num):
-        channel_list.append(int(feature_id / channel_windows_num))
-        point_start_list.append(int(feature_id % channel_windows_num * window_length))
-    return features_num, channel_list, point_start_list
+# def feature_segment(channels, points, window_length):
+#     channel_windows_num = int(points / window_length)  # 需要计算的通道特征数和时间特征数，总特征数为c_features x p_features
+#     features_num = channels * channel_windows_num
+#     channel_list, point_start_list = [], []
+#     for feature_id in range(features_num):
+#         channel_list.append(int(feature_id / channel_windows_num))
+#         point_start_list.append(int(feature_id % channel_windows_num * window_length))
+#     return features_num, channel_list, point_start_list
 
 
 def diff_shapley(data, model1, model2, window_length, M, NUM_CLASSES):
     n_samples, channels, points = data.shape
-    features_num, channel_list, point_start_list = feature_segment(channels, points, window_length)
+    features_num = (channels * points) // window_length
+    data = data.reshape((n_samples, channels * points))
 
-    S1 = np.zeros((n_samples, features_num, M, channels, points), dtype=np.float16)
-    S2 = np.zeros((n_samples, features_num, M, channels, points), dtype=np.float16)
+    all_sample_feature_maps = np.zeros((n_samples, features_num, NUM_CLASSES))
+    for index in tqdm(range(n_samples)):
+        S1 = np.zeros((features_num, M, channels * points), dtype=np.float16)
+        S2 = np.zeros((features_num, M, channels * points), dtype=np.float16)
+        for feature in range(features_num):
+            for m in range(M):
+                # 直接生成0，1数组，最后确保feature位满足要求，并且将数据类型改为Boolean型减少后续矩阵点乘计算量
+                feature_mark = np.random.randint(0, 2, features_num, dtype=np.bool_)  # bool_类型不能改为int8类型
+                feature_mark[feature] = 0
+                feature_mark = np.repeat(feature_mark, window_length)
+                # feature_mark = np.reshape(feature_mark, (channels, points))  # reshape是view，resize是copy
 
-    for feature in range(features_num):
-        for m in range(M):
-            # 直接生成0，1数组，最后确保feature位满足要求，并且将数据类型改为Boolean型减少后续矩阵点乘计算量
-            feature_mark = np.random.randint(0, 2, features_num, dtype=np.bool_)  # bool_类型不能改为int8类型
-            feature_mark[feature] = 0
-            feature_mark = np.repeat(feature_mark, window_length)
-            feature_mark = np.reshape(feature_mark, (channels, points))  # reshape是view，resize是copy
-            for index in range(n_samples):
                 # 随机选择一个参考样本，用于替换不考虑的特征核
                 reference_index = (index + np.random.randint(1, n_samples)) % n_samples
                 assert index != reference_index  # 参考样本不能是样本本身
                 reference_input = data[reference_index]
-                S1[index, feature, m] = S2[index, feature, m] = feature_mark * data[index] + ~feature_mark * reference_input
-                S1[index, feature, m][channel_list[feature],
-                point_start_list[feature]:point_start_list[feature] + window_length] = \
-                    data[index][channel_list[feature], point_start_list[feature]:point_start_list[feature] + window_length]
+                S1[feature, m] = S2[feature, m] = feature_mark * data[index] + ~feature_mark * reference_input
+                S1[feature, m][feature*window_length:(feature+1)*window_length] = data[index][feature*window_length:(feature+1)*window_length]
+                # S1[feature, m][channel_list[feature], point_start_list[feature]:point_start_list[feature] + window_length] = \
+                #     data[index][channel_list[feature], point_start_list[feature]:point_start_list[feature] + window_length]
 
-    # 计算S1和S2的预测差值
-    S1 = S1.reshape(-1, channels, points)
-    S2 = S2.reshape(-1, channels, points)
-    S1_preds = predict(model1, S1, NUM_CLASSES, eval=True) - predict(model2, S1, NUM_CLASSES, eval=True)
-    S2_preds = predict(model1, S2, NUM_CLASSES, eval=True) - predict(model2, S2, NUM_CLASSES, eval=True)
-    features = (S1_preds.view(n_samples, features_num, M, -1) -
-                S2_preds.view(n_samples, features_num, M, -1)).sum(axis=2) / M
-    return features.cpu().detach().numpy()
+        # 计算S1和S2的预测差值
+        S1 = S1.reshape(-1, channels, points)
+        S2 = S2.reshape(-1, channels, points)
+        S1_preds = predict(model1, S1, NUM_CLASSES, eval=True) - predict(model2, S1, NUM_CLASSES, eval=True)
+        S2_preds = predict(model1, S2, NUM_CLASSES, eval=True) - predict(model2, S2, NUM_CLASSES, eval=True)
+        sample_feature_maps = (S1_preds.view(features_num, M, -1) - S2_preds.view(features_num, M, -1)).sum(axis=1) / M
+
+        all_sample_feature_maps[index] = sample_feature_maps.cpu().detach().numpy()
+    return all_sample_feature_maps
 
 
 def diff_shapley_feature(data, model1, model2, window_length, M, NUM_CLASSES):
@@ -179,38 +182,35 @@ def diff_shapley_feature(data, model1, model2, window_length, M, NUM_CLASSES):
 
 def diff_shapley_parallel(data, model1, model2, window_length, M, NUM_CLASSES):
     n_samples, channels, points = data.shape
-    features_num, channel_list, point_start_list = feature_segment(channels, points, window_length)
+    features_num = (channels * points) // window_length
+    data = data.reshape((n_samples, channels * points))
 
     @ray.remote
-    def run(feature, data_sample_, data_ref_):
-        S1_r = np.zeros((M, channels, points), dtype=np.float16)
-        S2_r = np.zeros((M, channels, points), dtype=np.float16)
+    def run(feature, index, data_):
+        S1_r = np.zeros((M, channels * points), dtype=np.float16)
+        S2_r = np.zeros((M, channels * points), dtype=np.float16)
         for m in range(M):
             # 直接生成0，1数组，最后确保feature位满足要求，并且将数据类型改为Boolean型减少后续矩阵点乘计算量
             feature_mark = np.random.randint(0, 2, features_num, dtype=np.bool_)  # bool_类型不能改为int8类型
             feature_mark[feature] = 0
             feature_mark = np.repeat(feature_mark, window_length)
-            feature_mark = np.reshape(feature_mark, (channels, points))  # reshape是view，resize是copy
 
-            S1_r[m] = S2_r[m] = feature_mark * data_sample_ + ~feature_mark * data_ref_
-            S1_r[m][channel_list[feature], point_start_list[feature]:point_start_list[feature] + window_length] = \
-                data_sample_[channel_list[feature], point_start_list[feature]:point_start_list[feature] + window_length]
+            # 随机选择一个参考样本，用于替换不考虑的特征核
+            reference_index = (index + np.random.randint(1, n_samples)) % n_samples
+            assert index != reference_index  # 参考样本不能是样本本身
+
+            S1_r[m] = S2_r[m] = feature_mark * data_[index] + ~feature_mark * data_[reference_index]
+            S1_r[m][feature*window_length:(feature+1)*window_length] = \
+                data_[index][feature*window_length:(feature+1)*window_length]
         return feature, S1_r, S2_r
 
     all_sample_feature_maps = np.zeros((n_samples, features_num, NUM_CLASSES))
+    data_ = ray.put(data)
     for index in tqdm(range(n_samples)):
-        S1 = np.zeros((features_num, M, channels, points), dtype=np.float16)
-        S2 = np.zeros((features_num, M, channels, points), dtype=np.float16)
-        data_sample = data[index]
-        # 随机选择一个参考样本，用于替换不考虑的特征核
-        reference_index = (index + np.random.randint(1, n_samples)) % n_samples
-        assert index != reference_index  # 参考样本不能是样本本身
-        data_ref = data[reference_index]
+        S1 = np.zeros((features_num, M, channels * points), dtype=np.float16)
+        S2 = np.zeros((features_num, M, channels * points), dtype=np.float16)
 
-        data_sample_ = ray.put(data_sample)
-        data_ref_ = ray.put(data_ref)
-
-        rs = [run.remote(feature, data_sample_, data_ref_) for feature in range(features_num)]
+        rs = [run.remote(feature, index, data_) for feature in range(features_num)]
         rs_list = ray.get(rs)
         for feature, S1_r, S2_r in rs_list:
             S1[feature] = S1_r
