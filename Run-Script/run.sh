7d8edd02e52a9f33309392d143195eb7b8fa709e
hitfyd <hitfyd@foxmail.com>
2024-05-27 16:17:40 +0800

    运行脚本增加解释器轮询

--- Run-Script/run.sh
+++ Run-Script/run.sh
@@ -2,15 +2,19 @@
 
 dataset='DecMeg2014'  # CamCAN or DecMeg2014
 models=('mlp' 'sdt' 'lfcnn' 'varcnn' 'hgrn' 'atcnet') # 'mlp' 'sdt' 'lfcnn' 'varcnn' 'hgrn' 'atcnet'
-explainer='Logit_Aug_Sel' # 'SS' 'IMD' 'Delta' 'Logit' 'Logit_Aug' 'Logit_Sel' 'Logit_Aug_Sel'
+explainers=('SS' 'IMD' 'Delta' 'MERLIN' 'Logit' 'Logit_Aug' 'Logit_Sel' 'Logit_Aug_Sel')
 
-N=${#models[@]}
+N_M=${#models[@]}
+N_E=${#explainers[@]}
 
-for (( a=0 ; a<N-1 ; $(( a++ )) ))
+for (( a=0 ; a<N_M-1 ; $(( a++ )) ))
 do
-  for (( b=a+1; b<N; $(( b++)) ))
+  for (( b=a+1 ; b<N_M ; $(( b++)) ))
   do
-    echo ${dataset} ${explainer} "${models[a]}" "${models[b]}"
-    python difference_analysis.py --cfg ../configs/${dataset}/${explainer}.yaml MODEL_A "${models[a]}" MODEL_B "${models[b]}"
+    for (( c=0 ; c<N_E ; $(( c++ )) ))
+    do
+      echo ${dataset} "${explainers[c]}" "${models[a]}" "${models[b]}"
+      python difference_analysis.py --cfg ../configs/${dataset}/"${explainers[c]}".yaml MODEL_A "${models[a]}" MODEL_B "${models[b]}"
+    done
   done
 done
