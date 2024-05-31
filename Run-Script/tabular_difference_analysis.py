96dd06f9000188bc9caa9a8bd426449b4db758bd
hitfyd <hitfyd@foxmail.com>
2024-05-31 17:08:00 +0800

    在两个表格数据集上的多次平均测试

--- Run-Script/tabular_difference_analysis.py
+++ Run-Script/tabular_difference_analysis.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -16,7 +17,8 @@ from differlib.explainer.imd.utils import load_bc_dataset, load_waveform_dataset
 # Data preparation
 random_state = 1234
 train_size = 0.7
-max_depth = 5
+n_times = 5
+max_depth = 4
 min_samples_leaf = 1
 datasets = {'bc': load_bc_dataset(),
             'waveform': load_waveform_dataset(),
@@ -55,10 +57,12 @@ for dataset in datasets.keys():
         models[model_name] = model
 
     n_models = len(models.keys())
+    model_types_list = list(models.keys())
     models_list = list(models.values())
     for i in range(n_models-1):
         for j in range(i+1, n_models):
             model1, model2 = models_list[i], models_list[j]
+            model1_name, model2_name = model_types_list[i], model_types_list[j]
 
             # Calculate diff-samples %
             # feature_names = x_train.columns.to_list()
@@ -78,16 +82,59 @@ for dataset in datasets.keys():
             print(f"diffs in X_test = {ydifftest.sum()} / {len(ydifftest)} = {(ydifftest.sum() / len(ydifftest)):.2f}")
 
             for explainer_type in explainers.keys():
-                explainer = explainers[explainer_type]()
-                if explainer_type in ["Logit"]:
-                    explainer.fit(x_train, output1, output2, max_depth, min_samples_leaf=min_samples_leaf,)
-                    train_metrics = explainer.metrics(x_train, output1, output2, name="train")
-                    test_metrics = explainer.metrics(x_test, t_output1, t_output2)
-                else:
-                    explainer.fit(x_train, y1, y2, max_depth, min_samples_leaf=min_samples_leaf)
-                    train_metrics = explainer.metrics(x_train, y1, y2, name="train")
-                    test_metrics = explainer.metrics(x_test, t_y1, t_y2)
+                pd_test_metrics, pd_train_metrics = None, None
+                for skf_id in range(n_times):
+                    # x_train, x_test, y_train, y_test = train_test_split(data, target, train_size=train_size)
+                    explainer = explainers[explainer_type]()
+                    if explainer_type in ["Logit"]:
+                        explainer.fit(x_train, output1, output2, max_depth, min_samples_leaf=min_samples_leaf,)
+                        train_metrics = explainer.metrics(x_train, output1, output2, name="train")
+                        test_metrics = explainer.metrics(x_test, t_output1, t_output2)
+                    else:
+                        explainer.fit(x_train, y1, y2, max_depth, min_samples_leaf=min_samples_leaf)
+                        train_metrics = explainer.metrics(x_train, y1, y2, name="train")
+                        test_metrics = explainer.metrics(x_test, t_y1, t_y2)
 
-                print(dataset, explainer_type, model1, model2)
-                print(train_metrics)
-                print(test_metrics)
+                    # 打印单次实验结果
+                    print(dataset, explainer_type, model1, model2, skf_id)
+                    print(train_metrics)
+                    print(test_metrics)
+
+                    # 记录单次实验的训练和测试结果
+                    train_metrics['train-confusion_matrix'] = np.array2string(train_metrics['train-confusion_matrix'])
+                    if pd_train_metrics is None:
+                        pd_train_metrics = pd.DataFrame(columns=train_metrics.keys())
+                    pd_train_metrics.loc[len(pd_train_metrics)] = train_metrics.values()
+
+                    test_metrics['test-confusion_matrix'] = np.array2string(test_metrics['test-confusion_matrix'])
+                    if pd_test_metrics is None:
+                        pd_test_metrics = pd.DataFrame(columns=test_metrics.keys())
+                    pd_test_metrics.loc[len(pd_test_metrics)] = test_metrics.values()
+
+                # 计算测试集上各个指标的均值和标准差
+                assert len(pd_test_metrics.columns.tolist()) == 10
+                partial_pd_metrics = pd_test_metrics.iloc[:, 3:]
+                partial_pd_metrics_mean, partial_pd_metrics_std = partial_pd_metrics.mean(), partial_pd_metrics.std()
+                record_mean_std = pd.Series(index=partial_pd_metrics_mean.index, dtype=str)
+                for v in range(len(partial_pd_metrics_mean.values)):
+                    record_mean_std.iloc[v] = f"{partial_pd_metrics_mean.iloc[v]:.2f} ± {partial_pd_metrics_std.iloc[v]:.2f}"
+                print(record_mean_std.to_string())
+                with open(os.path.join(log_path, "worklog.txt"), "a") as writer:
+                    writer.write(os.linesep + "-" * 25 + os.linesep)
+                    writer.write(pd_train_metrics.to_string() + os.linesep)
+                    writer.write(pd_test_metrics.to_string() + os.linesep)
+                    writer.write(record_mean_std.to_string() + os.linesep)
+                    writer.write(os.linesep + "-" * 25 + os.linesep)
+
+                # 根据模型A、B，记录不同解释器配置下的测试集实验结果用于对比
+                record_file = os.path.join(log_path, f"{dataset}_{model1_name}_{model2_name}_record.csv")
+                record_mean_std['model1'] = model1_name
+                record_mean_std['model2'] = model2_name
+                record_mean_std['explainer'] = explainer_type
+                if os.path.exists(record_file):
+                    all_record_mean_std = pd.read_csv(record_file)
+                    assert all_record_mean_std.columns.tolist() == record_mean_std.index.tolist()
+                else:
+                    all_record_mean_std = pd.DataFrame(columns=record_mean_std.index)
+                all_record_mean_std.loc[len(all_record_mean_std)] = record_mean_std.values
+                all_record_mean_std.to_csv(record_file, index=False)
