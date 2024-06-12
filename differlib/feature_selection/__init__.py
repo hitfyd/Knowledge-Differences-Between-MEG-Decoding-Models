e393c1168ee61db568bc4adb0f3e790f4ef69b7a
hitfyd <hitfyd@foxmail.com>
2024-06-12 15:24:25 +0800

    删除遗留代码

--- differlib/feature_selection/__init__.py
+++ differlib/feature_selection/__init__.py
@@ -1,13 +1,7 @@
 from .DiffShapleyFS import DiffShapleyFS
-from .LinearRegression import LinearRegression, SGDRegressor
-from .borutaFS import BorutaFS
-from .fsm import NoneFSM, RandFSM
+from .fsm import NoneFSM
 
 fsm_dict = {
     "NONE": NoneFSM,
-    "Rand": RandFSM,
-    "LR": LinearRegression,
-    "SGDR": SGDRegressor,
-    "Boruta": BorutaFS,
     "DiffShapley": DiffShapleyFS,
 }
