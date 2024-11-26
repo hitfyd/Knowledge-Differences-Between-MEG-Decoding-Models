02e486f3899968c0941cc032c31ee0307fad5540
hitfyd <hitfyd@foxmail.com>
2024-11-26 15:58:39 +0800

    放弃token下载，完全浏览器下载

---
+++ dataset/get_ebrians.py
@@ -0,0 +1,28 @@
+import os
+
+from ebrains_drive import BucketApiClient
+
+dataset_id = "d55146e8-fc86-44dd-95db-7191fdca7f30"
+token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJLYU01NTRCM2RmMHBIamZYWi1aRl94bUUwMThPS1R0RkNjMjR3aVVqQmFvIn0.eyJleHAiOjE3MzI2MzQyNjQsImlhdCI6MTczMjU5MTA2MywiYXV0aF90aW1lIjoxNzMyNTAzNjQ5LCJqdGkiOiJiYjNiYmRiMS1iNWUzLTQ5Y2QtYjU4My0zODdlZTVjMWFjOWUiLCJpc3MiOiJodHRwczovL2lhbS5lYnJhaW5zLmV1L2F1dGgvcmVhbG1zL2hicCIsImF1ZCI6WyJqdXB5dGVyaHViLWpzYyIsInh3aWtpIiwidGVhbSIsImdyb3VwIl0sInN1YiI6IjJiYzcxNjIwLWI3NzktNGM3NS1hNGE4LWFiZTkxMTIxODBkNSIsInR5cCI6IkJlYXJlciIsImF6cCI6Imp1cHl0ZXJodWIiLCJzZXNzaW9uX3N0YXRlIjoiNjE4ZTJkYzgtYzMyOS00OTYwLTgyN2ItMTc2ZTJhM2FhZjJlIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vanVweXRlcmh1Yi5hcHBzLmpzYy5oYnAuZXUvIiwiaHR0cHM6Ly9sYWIuZWJyYWlucy5ldS8iLCJodHRwczovL2xhYi5qc2MuZWJyYWlucy5ldS8iXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIl19LCJzY29wZSI6ImNvbGxhYi5kcml2ZSBwcm9maWxlIG9mZmxpbmVfYWNjZXNzIGNsYi53aWtpLndyaXRlIGVtYWlsIHJvbGVzIG9wZW5pZCBncm91cCBjbGIud2lraS5yZWFkIHRlYW0iLCJzaWQiOiI2MThlMmRjOC1jMzI5LTQ5NjAtODI3Yi0xNzZlMmEzYWFmMmUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Illvbmdkb25nIEZhbiIsIm1pdHJlaWQtc3ViIjoiMzIyNjAzMjgyODk3NjI3NCIsInByZWZlcnJlZF91c2VybmFtZSI6ImhpdGZ5ZCIsImdpdmVuX25hbWUiOiJZb25nZG9uZyIsImZhbWlseV9uYW1lIjoiRmFuIiwiZW1haWwiOiJoaXRmeWRAaGl0LmVkdS5jbiJ9.FuMzdejgWGmLJAb9fVAQdRhMPbwDJo2Aeqr-5MCA_VIbFVwvGlPVREQeeVHgUlJQDloeayubCVaZqGglr2NYxXCEeyirbjzsd3dwbOd11d9ci7SKV36VHeQ6re87lmGrozeb-RjHcSWuBIWuI__KyG6jVcgoKmeM-1ikb_00rk3vqPammoGam5GjpJMf_LQZiJceusim4-a8sPfoWSue5G8iq9OjGtSBxXpN3g1BsCcG04y0m6WJ_eIapSQv7wKNsJxXjHHZgZwCfe5ZvBDPaR8aFigh5iQU557UXGm_V4ugmGqP6JbzRN66nTpHR8rHUo146wHi25wbsz4TdfxhBQ"
+root_dir = "D:/d55146e8-fc86-44dd-95db-7191fdca7f30"
+
+client = BucketApiClient(token=token)
+
+# access dataset bucket
+bucket = client.buckets.get_dataset(dataset_id)
+
+# for file in bucket.ls(prefix="sub-004"):
+for file in bucket.ls():
+    # 保存文件到本地
+    path = os.path.join(root_dir, file.name)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    if os.path.exists(path):
+        print(file.name, "Exists")
+        continue
+
+    file_handle = bucket.get_file(file.name)
+    file_content = file_handle.get_content(progress=True)
+    with open(path, 'wb') as f:
+        f.write(file_content)
+    print(file.name, "Done")
