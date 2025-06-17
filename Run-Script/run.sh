b70ac3e5d9c146329a829c4ed2c7736aebf72d5b
hitfyd <hitfyd@foxmail.com>
2025-06-17 19:31:39 +0800

    修复run.sh

--- Run-Script/run.sh
+++ Run-Script/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dataset='DecMeg2014'  # CamCAN or DecMeg2014
-models=('mlp' 'lfcnn' 'varcnn' 'hgrn' 'ctnet') # 'gnb' 'rf' 'lr' 'mlp' 'lfcnn' 'varcnn' 'hgrn' 'atcnet'
+models=('rf' 'mlp' 'lfcnn' 'varcnn' 'hgrn' 'ctnet') # 'gnb' 'rf' 'lr' 'mlp' 'lfcnn' 'varcnn' 'hgrn' 'atcnet'
 explainers=('Logit' 'Delta' 'MERLIN' 'SS' 'IMD')  # 'SS' 'IMD' 'Delta' 'MERLIN' 'Logit'
 
 N_M=${#models[@]}
@@ -14,7 +14,7 @@ do
     for (( c=0 ; c<N_E ; $(( c++ )) ))
     do
       echo ${dataset} "${explainers[c]}" "${models[a]}" "${models[b]}"
-      python difference_analysis.py --cfg ../configs/${dataset}/"${explainers[c]}".yaml MODEL_A "${models[a]}" MODEL_B "${models[b]}"
+      python meg_difference_analysis.py --cfg ../configs/${dataset}/"${explainers[c]}".yaml MODEL_A "${models[a]}" MODEL_B "${models[b]}"
     done
   done
 done
