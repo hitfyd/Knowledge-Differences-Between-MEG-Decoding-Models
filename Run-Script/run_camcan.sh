76a3c5924d16400e1546b691d6d2203ee22e4b56
hitfyd <hitfyd@foxmail.com>
2025-06-26 00:21:23 +0800

    两个MEG数据集脚本独立

---
+++ Run-Script/run_camcan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dataset='CamCAN'  # CamCAN or DecMeg2014
+models=('rf' 'mlp' 'lfcnn' 'varcnn' 'hgrn' 'atcnet' 'ctnet') # 'gnb' 'rf' 'lr' 'mlp' 'lfcnn' 'varcnn' 'hgrn' 'atcnet'
+AUGMENTATION=('Counterfactual' 'NONE')  # 'Counterfactual' 'NONE'
+
+N_M=${#models[@]}
+N_E=${#explainers[@]}
+
+for (( a=0 ; a<N_M-1 ; $(( a++ )) ))
+do
+  for (( b=a+1 ; b<N_M ; $(( b++)) ))
+  do
+    echo ${dataset} "${models[a]}" "${models[b]}"
+    python meg_difference_analysis.py --cfg ../configs/${dataset}/Benchmark.yaml MODEL_A "${models[a]}" MODEL_B "${models[b]}"
+  done
+done
